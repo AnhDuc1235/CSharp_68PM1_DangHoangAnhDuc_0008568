b6ac06088fe2f3618f60005dfad24cae287ce768
AnhDuc1235 <anhducdanghoang@gmail.com>
2026-05-13 21:36:04 +0700

    chức năng đăng nhập

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = textBox1.Text;
+            string password = textBox2.Text;
+            if (username == "0008568@st.huce.edu.vn" && password == "0008568")
+            {
+                MessageBox.Show("đăng nhập thành công");
+            }
+            else
+            {
+                MessageBox.Show("đăng nhập thất bại");
+            }
+        }
+    }
+}
